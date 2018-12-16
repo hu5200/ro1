effdff80b67ccf92e35d0d2edc02b2fb933933a0
hu5200 <45910451+hu5200@users.noreply.github.com>
2018-12-16 19:18:41 +0800

    Update FristTest.java

--- gitdemo/src/FristTest.java
+++ gitdemo/src/FristTest.java
@@ -1,5 +1,8 @@
 public class FristTest {
     public static void main(String[] args) {
         System.out.println("22222222222222");
+        System.out.println("22222222222222"); 
+         System.out.println("22222222222222");
+         System.out.println("22222222222222");
     }
 }
